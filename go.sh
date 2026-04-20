30c3b3eafa1e223d573e18a505c927bec085b19a
Dave Woods <79603573+dwoodshc@users.noreply.github.com>
2026-04-20 17:40:24 +0100

    Improved Installer

--- go.sh
+++ go.sh
@@ -5,29 +5,65 @@
 ###############################################################################
 
 images=(
-    example-microservice_api
-    example-microservice_frontend
-    example-microservice_quotes
-    example-microservice_test
-    example-microservice_news
+    example-microservice-api
+    example-microservice-frontend
+    example-microservice-quotes
+    example-microservice-test
+    example-microservice-news
 )
 
 clear
+echo " "
 echo "========================================================================="
 echo "Full Rebuild of all Images and Containers"
 echo "========================================================================="
+echo " "
 
-for image in ${images[@]}
+# Examples of API Gateway URLs:
+# API_GATEWAY=http://localhost:3000
+# API_GATEWAY=http://192.168.1.176:3000
+
+if [ $API_GATEWAY ]; then
+  echo "OK: Using API Gateway at ${API_GATEWAY}"
+else
+  echo "ERROR: No API Gateway specified...exiting"
+  exit
+fi
+echo " "
+
+
+echo "-------------------------------------------------------------------------"
+echo "- Image Cleanup"
+echo "-------------------------------------------------------------------------"
+for image in "${images[@]}"
 do
-  echo "Removing [$image]"
-  docker container rm ${image}_1
-  docker image rm ${image}
+  echo "++++ Processing [$image]"
+  pid=$(docker ps -a --filter="name=$image" --format="{{.ID}}")
+ 
+  if [ "$pid" != "" ]
+  then
+    echo "Container [$image] is running with PID [$pid]...stopping and removing"
+    docker container rm -f "${image}-1"
+  else
+    echo "Container [$image] is not running...no need to stop or remove"
+  fi
+  docker rmi -f "$image"
+  echo " "
+  echo " "
+
+
 done
 
-API_GATEWAY=http://localhost:3000
 
 cd FrontendApplication/
 npm run build
 cd ..
 
-docker-compose up
+docker-compose up &
+sleep 5
+
+
+echo "========================================================================="
+echo "All done!"
+echo "Navigate to http://localhost:8080 to see the frontend application"
+echo "========================================================================="  
